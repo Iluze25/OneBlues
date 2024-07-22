70d704060100de6c0950d8c6128a8db65f2f2589
Iluze Ear <nsumarna150@gmail.com>
2024-07-23 00:56:47 +0700

    Hello

--- HappyBirthday/script.js
+++ HappyBirthday/script.js
@@ -1,11 +1,12 @@
 $("#hhg").on("click", function() {
     // Saya belum mencoba code ini
-    $("#edan").attr("src", "/HappyBirthday/Figure/20180423WantotVsRouf.mp4")
+    $("#edan").attr("src", "Figure/20180423WantotVsRouf.mp4")
     document.getElementById('edan').play()
+    $('p').text('Hello World').hide().fadeIn(4000);
     setTimeout(function(){
         $("#edan").attr("src", "Figure/VID-20230315-WA0016.mp4")
         document.getElementById('edan').play()
-    }, 29000)
+    }, 30000)
 })
 
 $("#hhg").on("click", function(){
